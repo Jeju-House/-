aadf5b6d588ee82a2afbc705e528548d0ea6b1bd
Bae Hyeonseo <haulqogustj@naver.com>
2024-01-14 16:19:06 +0900

    [Main]: exception추가

---
+++ main/main/src/main/java/com/jeju/main/global/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.jeju.main.global.exception;
+
+import com.jeju.main.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
---
+++ main/main/src/main/java/com/jeju/main/global/exception/UnauthorizedException.java
@@ -0,0 +1,14 @@
+package com.jeju.main.global.exception;
+
+
+import com.jeju.main.global.error.ErrorCode;
+
+public class UnauthorizedException extends BusinessException {
+    public UnauthorizedException() {
+        super(ErrorCode.UNAUTHORIZED);
+    }
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
