736c468fde934bc7edf76ad63ddcabba3d3df348
Bae Hyeonseo <haulqogustj@naver.com>
2024-01-25 22:12:28 +0900

    [Main]: reservation api

--- main/main/src/main/java/com/jeju/main/domain/reservation/service/ReservationService.java
+++ main/main/src/main/java/com/jeju/main/domain/reservation/service/ReservationService.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
+    public void reservation(Long userId, Long guestHouseId){
+
+    }
 }
